a3aa2e89b2c14ad44de5f34a66743309c13ec2c7
bobby <bobby@debian-BULLSEYE-live-builder-AMD64>
2022-09-09 23:13:36 -0500

    initial commit #2

---
+++ initgit.sh
@@ -0,0 +1,1 @@
+gh repo create hellopythonch6 --public; git init; git add README.md; git commit -m first commit; git remote add origin git@github.com:robjohncolson/hellopythonch6.git; git push -u origin master
